590f81998bf9c571584fd644c544616155f873d5
mingkao92 <gmatwy@gmail.com>
2017-07-18 23:37:34 +0800

    test

---
+++ core/Logic/User.php
@@ -0,0 +1,54 @@
+<?php 
+namespace Core\Logic;
+use Interaction\BaseLogic;
+
+class User extends BaseLogic
+{
+
+	/**
+	 * store self
+	 * @var BaseLogic
+	 */
+	protected $user;
+
+	/**
+	 * must be overidded
+	 * @var boolean
+	 */
+	protected $has_login;
+
+	/**
+	 * must be overidded
+	 * @var boolean
+	 */
+	protected $is_adult;
+
+	/**
+	 * action
+	 * @var string
+	 */
+	protected $accessed;
+
+	/**
+	 * action
+	 * @var [type]
+	 */
+	protected $forbidden;
+
+
+	public function __construct()
+	{
+		$this->user = new self();
+	}
+
+	public function access()
+	{
+		if ($this->user->has_login) {
+			
+		} else {
+			
+		}
+	}
+}
+
+ ?>
